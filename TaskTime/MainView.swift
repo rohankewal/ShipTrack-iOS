346e808cacca0429abe33af2586359609d522859
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2023-11-02 20:07:23 -0400

    created main view screen

--- TaskTime/MainView.swift
+++ TaskTime/MainView.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("ShipTrack")
+            .font(.title)
+            .bold()
+
+        TabView {
+            EmptyView()
+                .badge(2)
+                .tabItem {
+                    Label("Received", systemImage: "house.fill")
+                }
+            EmptyView()
+                .tabItem {
+                    Label("Sent", systemImage: "map.fill")
+                }
+            EmptyView()
+                .badge("!")
+                .tabItem {
+                    Label("Account", systemImage: "gear")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
