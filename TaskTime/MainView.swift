290334beecd72b51b35fbe45cbed06a5fe4da9ff
Rohan Kewalramani <45906368+rohankewal@users.noreply.github.com>
2023-11-02 21:27:01 -0400

    added map to main view

--- TaskTime/MainView.swift
+++ TaskTime/MainView.swift
@@ -7,13 +7,27 @@
 
 import Foundation
 import SwiftUI
+import MapKit
 
 struct MainView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
     var body: some View {
         Text("ShipTrack")
             .font(.title)
             .bold()
-
+        
+        ZStack {
+            Map(coordinateRegion: $region)
+                .cornerRadius(20)
+                .shadow(radius: 10)
+                .padding()
+            // You can add more styling to make it look like a card
+        }
+        
         TabView {
             EmptyView()
                 .badge(2)
